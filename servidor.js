2be756d631c428d5120c5f9c0e48cde1384116b5
perico <danilo.perico@gmail.com>
2018-04-27 15:01:57 -0300

    fisrt commit

---
+++ servidor.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('./public/html'));
+
+app.post('/cadastra', function(requisicao, resp){
+    var nome = requisicao.body.nome; 
+    var login = requisicao.body.login;
+    resp.render('resposta', {nome, login})
+});
+
+
+var servidor = http.createServer(app);
+servidor.listen(8080);
+
+console.log("servidor rodando...");
